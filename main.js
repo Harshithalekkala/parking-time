e0220101ef096707e3fdaf7e78610d592a7fa0a0
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2021-10-25 20:51:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 canvas= document.getElementById('myCanvas');
 ctx= canvas.getContext("2d");
 
-greencar_width=75;
-green_height=100;
+greencar_width=80;
+green_height=80;
 
 background_image = "parkingLot.jpg";
 greencar_image = "car2.png";
@@ -13,7 +13,7 @@ greencar_y=225;
 function add() {
 	background_imgTag=new Image();
 	background_imgTag.onload=uploadBackground;
-	background_imgTag.src=greencar_image;
+	background_imgTag.src=background_image;
 
 	greencar_imgTag=new Image();
 	greencar_imgTag.onload=uploadgreencar;
@@ -21,7 +21,7 @@ function add() {
 }
 
 function uploadBackground() {
-	ctx.drawImage(background_imgTag,0,0,canvas.width, canvas.height);z
+	ctx.drawImage(background_imgTag,0,0,canvas.width, canvas.height);
 
 }
 
@@ -77,15 +77,30 @@ function up()
 
 function down()
 {
-	
+	if (greencar_y <=500) {
+		greencar_y=greencar_y+10;
+		console.log("When up arrow is pressed,  x = " +  greencar_x +  "| y = " +greencar_y);	
+		uploadBackground();
+		uploadgreencar();
+		}	
 }
 
 function left()
 {
-	//Define function to move the car left side
+	if (greencar_x >=0) {
+		greencar_x=greencar_x-10;
+		console.log("When up arrow is pressed,  x = " +  greencar_x +  "| y = " +greencar_y);	
+		uploadBackground();
+		uploadgreencar();
+		}	
 }
 
 function right()
 {
-	//Define function to move the car right side
+	if (greencar_x <=800) {
+		greencar_x=greencar_x+10;
+		console.log("When up arrow is pressed,  x = " +  greencar_x +  "| y = " +greencar_y);	
+		uploadBackground();
+		uploadgreencar();
+		}	
 }
